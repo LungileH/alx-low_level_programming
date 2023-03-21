18d1485cd212fa510b87523a733e567cb32ff739
LungileH <lungileH700@gmail.com>
2023-03-21 09:31:42 -0700

    Lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_alphabet - function that prints alphabet in lowercase
+* Return: always 0
+*/
+
+void print_alphabet(void)
+{
+		char letter;
+
+
+		for (letter = 'a'; letteri <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
+	}
